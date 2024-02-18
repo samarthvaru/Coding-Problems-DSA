6854d238f0520a6b60214ae39135a7669fb3fe5b
Samarth Varu <samarthvaru111@gmail.com>
2024-02-18 18:34:16 -0500

    some heap problems

---
+++ kth_largest_element_array.py
@@ -0,0 +1,10 @@
+# Solution: Sorting
+# Time Complexity:
+#   - Best Case: O(n)
+#   - Average Case: O(n*log(n))
+#   - Worst Case:O(n*log(n))
+# Extra Space Complexity: O(n)
+class Solution1:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        return nums[len(nums) - k]
